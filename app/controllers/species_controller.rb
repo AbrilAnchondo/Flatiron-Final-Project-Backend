f74ec812d0b535eedb7d6354ab9f46a722fd600d
Abril Anchondo Reynaga <abril79reynaga@gmail.com>
2019-09-09 01:05:09 -0400

    added to seed file

--- app/controllers/species_controller.rb
+++ app/controllers/species_controller.rb
@@ -4,7 +4,8 @@ class SpeciesController < ApplicationController
     def index
        if authenticated? # see application_controller.rb
           species = Species.all
-          render json: species, include: :followings
+          render json: species, :include => [:followings, :users, :comments => {:include => :user}]
+          #render json: species, include: :followings
        else
          tell_user_to_go_away! # see application_controller.rb
         end
