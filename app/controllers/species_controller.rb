0fc213ed684efbd3a6aa4f08e3697d70226cd486
Abril Anchondo Reynaga <abril79reynaga@gmail.com>
2019-09-11 01:49:30 -0400

    slight changes to render json species controller

--- app/controllers/species_controller.rb
+++ app/controllers/species_controller.rb
@@ -4,8 +4,8 @@ class SpeciesController < ApplicationController
     def index
        if authenticated? # see application_controller.rb
           species = Species.all
-          render json: species, :include => [:followings, :users, :comments => {:include => :user}]
-          #render json: species, include: :followings
+          render json: species, :include => [:users, :followings =>{:include => :user}, :comments => {:include => :user}]
+          
        else
          tell_user_to_go_away! # see application_controller.rb
         end
@@ -14,20 +14,7 @@ class SpeciesController < ApplicationController
       def show
         if authenticated?
             species = Species.find(params[:id])
-            render json: species, :include => [:followings, :users, :comments => {:include => :user}]
+            render json: species, :include => [:users, :followings =>{:include => :user}, :comments => {:include => :user}]
          end
       end
-
-     
-
-      # example to create associations, you have to send the token otherwise you cannot make that association
-      # creating a species_user
-      # def species_user
-      #    if authenticated?
-      #       UserSpecies.create(user: user_who_is_logged_in, species_id: params[:id])
-      #       #user_who_is_logged_in.user_species.create(species_id: 2)
-      #    end
-      # end
-
-    
 end
